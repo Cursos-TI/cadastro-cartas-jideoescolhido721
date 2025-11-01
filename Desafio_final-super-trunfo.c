11ebdf29feb9dde90317ea527bef255b560e80db
Hacker Jeday$7 <messiasoescolhido@hotmail.com>
2025-11-01 01:07:23 +0000

    Switch e menus dinamicos, operadores ternarios e dois atributos para os jogadores escolherem suas cartas

--- Desafio_final-super-trunfo.c
+++ Desafio_final-super-trunfo.c
@@ -1,96 +1,113 @@
 #include <stdio.h>
-#include <string.h>
-
-#define NUM_ATRIBUTOS 4
-
-typedef struct {
-    char nome[50];
-    int populacao;
-    int area;
-    int densidade;
-    int pib;
-} Carta;
-
-void exibirAtributosDisponiveis(int escolhido) {
-    printf("Escolha um atributo:\n");
-    if (escolhido != 1) printf("1 - Populacao\n");
-    if (escolhido != 2) printf("2 - Area\n");
-    if (escolhido != 3) printf("3 - Densidade Demografica\n");
-    if (escolhido != 4) printf("4 - PIB\n");
-}
-
-int obterValorAtributo(Carta c, int atributo) {
-    switch (atributo) {
-        case 1: return c.populacao;
-        case 2: return c.area;
-        case 3: return c.densidade;
-        case 4: return c.pib;
-        default: return -1;
-    }
-}
-
-int compararAtributo(int valor1, int valor2, int atributo) {
-    return (atributo == 3) ? (valor1 < valor2 ? 1 : (valor1 > valor2 ? 2 : 0)) :
-                             (valor1 > valor2 ? 1 : (valor1 < valor2 ? 2 : 0));
-}
+#include <stdlib.h>
+#include <time.h>
 
 int main() {
-    Carta carta1 = {"Brasil", 213000000, 8516000, 25, 2200};
-    Carta carta2 = {"Japao", 125800000, 377975, 347, 5000};
+    int resultado1, resultado2;
+    char primeiroAtributo, segundoAtributo;
+    int ataque1, ataque2, defesa1, defesa2, recuo1, recuo2;
+    int soma1, soma2;
 
-    int atributo1, atributo2;
+    // Gerar número aleatório
+    srand(time(0));
+    ataque1 = rand() % 100 + 1;
+    ataque2 = rand() % 100 + 1;
+    defesa1 = rand() % 100 + 1;
+    defesa2 = rand() % 100 + 1;
+    recuo1  = rand() % 100 + 1;
+    recuo2  = rand() % 100 + 1;
 
-    printf("Comparacao entre %s e %s\n\n", carta1.nome, carta2.nome);
+    printf("Bem-vindo ao jogo!\n");
+    printf("Valores das cartas:\n");
+    printf("Carta 1 - Ataque: %d | Defesa: %d | Recuo: %d\n", ataque1, defesa1, recuo1);
+    printf("Carta 2 - Ataque: %d | Defesa: %d | Recuo: %d\n", ataque2, defesa2, recuo2);
 
     // Escolha do primeiro atributo
-    exibirAtributosDisponiveis(0);
-    scanf("%d", &atributo1);
-
-    // Escolha do segundo atributo
     do {
-        exibirAtributosDisponiveis(atributo1);
-        scanf("%d", &atributo2);
-        if (atributo2 == atributo1) {
-            printf("Voce ja escolheu esse atributo. Escolha outro.\n");
-        }
-    } while (atributo2 == atributo1);
+        printf("\nEscolha o primeiro Atributo:\n");
+        printf("A - Ataque\nD - Defesa\nR - Recuo\n");
+        printf("Digite sua escolha: ");
+        scanf(" %c", &primeiroAtributo);
+        primeiroAtributo = toupper(primeiroAtributo);
+        if(primeiroAtributo != 'A' && primeiroAtributo != 'D' && primeiroAtributo != 'R')
+            printf("Opção inválida!\n");
+    } while(primeiroAtributo != 'A' && primeiroAtributo != 'D' && primeiroAtributo != 'R');
 
-    // Obter valores
-    int valor1_attr1 = obterValorAtributo(carta1, atributo1);
-    int valor2_attr1 = obterValorAtributo(carta2, atributo1);
-    int valor1_attr2 = obterValorAtributo(carta1, atributo2);
-    int valor2_attr2 = obterValorAtributo(carta2, atributo2);
+    // Escolha do segundo atributo - dinâmico!
+    do {
+        printf("\nEscolha o segundo Atributo (diferente do primeiro):\n");
+        if(primeiroAtributo != 'A') printf("A - Ataque\n");
+        if(primeiroAtributo != 'D') printf("D - Defesa\n");
+        if(primeiroAtributo != 'R') printf("R - Recuo\n");
+        printf("Digite sua escolha: ");
+        scanf(" %c", &segundoAtributo);
+        segundoAtributo = toupper(segundoAtributo);
+        if(segundoAtributo == primeiroAtributo || 
+           (segundoAtributo != 'A' && segundoAtributo != 'D' && segundoAtributo != 'R'))
+            printf("Opção inválida ou repetida! Tente novamente.\n");
+    } while(segundoAtributo == primeiroAtributo || 
+            (segundoAtributo != 'A' && segundoAtributo != 'D' && segundoAtributo != 'R'));
 
-    // Comparacoes individuais
-    int resultado1 = compararAtributo(valor1_attr1, valor2_attr1, atributo1);
-    int resultado2 = compararAtributo(valor1_attr2, valor2_attr2, atributo2);
+    // Switch para comparar atributos, usando operador ternário
+    switch(primeiroAtributo) {
+        case 'A':
+            resultado1 = (ataque1 > ataque2) ? 1 : (ataque1 < ataque2 ? 2 : 0);
+            soma1 = ataque1;
+            soma2 = ataque2;
+            break;
+        case 'D':
+            resultado1 = (defesa1 > defesa2) ? 1 : (defesa1 < defesa2 ? 2 : 0);
+            soma1 = defesa1;
+            soma2 = defesa2;
+            break;
+        case 'R':
+            // Exemplo: para recuo vence menor valor
+            resultado1 = (recuo1 < recuo2) ? 1 : (recuo1 > recuo2 ? 2 : 0);
+            soma1 = recuo1;
+            soma2 = recuo2;
+            break;
+        default:
+            printf("Erro inesperado!\n");
+    }
 
-    // Soma dos atributos
-    int soma1 = valor1_attr1 + valor1_attr2;
-    int soma2 = valor2_attr1 + valor2_attr2;
+    switch(segundoAtributo) {
+        case 'A':
+            resultado2 = (ataque1 > ataque2) ? 1 : (ataque1 < ataque2 ? 2 : 0);
+            soma1 += ataque1;
+            soma2 += ataque2;
+            break;
+        case 'D':
+            resultado2 = (defesa1 > defesa2) ? 1 : (defesa1 < defesa2 ? 2 : 0);
+            soma1 += defesa1;
+            soma2 += defesa2;
+            break;
+        case 'R':
+            resultado2 = (recuo1 < recuo2) ? 1 : (recuo1 > recuo2 ? 2 : 0);
+            soma1 += recuo1;
+            soma2 += recuo2;
+            break;
+        default:
+            printf("Erro inesperado!\n");
+    }
 
-    // Resultado final
-    printf("\nResultado da comparacao:\n");
-    printf("%s - %s: %d, %s: %d | Soma: %d\n", carta1.nome,
-           atributo1 == 1 ? "Populacao" : atributo1 == 2 ? "Area" : atributo1 == 3 ? "Densidade" : "PIB",
-           valor1_attr1,
-           atributo2 == 1 ? "Populacao" : atributo2 == 2 ? "Area" : atributo2 == 3 ? "Densidade" : "PIB",
-           valor1_attr2,
-           soma1);
+    // Exibição clara dos resultados
+    printf("\nResultados das Comparações:\n");
+    printf("Primeiro atributo (%c): Carta 1 = %d, Carta 2 = %d\n", primeiroAtributo,
+            (primeiroAtributo=='A'?ataque1:(primeiroAtributo=='D'?defesa1:recuo1)),
+            (primeiroAtributo=='A'?ataque2:(primeiroAtributo=='D'?defesa2:recuo2)));
+    printf("Segundo atributo (%c): Carta 1 = %d, Carta 2 = %d\n", segundoAtributo,
+            (segundoAtributo=='A'?ataque1:(segundoAtributo=='D'?defesa1:recuo1)),
+            (segundoAtributo=='A'?ataque2:(segundoAtributo=='D'?defesa2:recuo2)));
+    printf("Soma dos atributos: Carta 1 = %d, Carta 2 = %d\n", soma1, soma2);
 
-    printf("%s - %s: %d, %s: %d | Soma: %d\n", carta2.nome,
-           atributo1 == 1 ? "Populacao" : atributo1 == 2 ? "Area" : atributo1 == 3 ? "Densidade" : "PIB",
-           valor2_attr1,
-           atributo2 == 1 ? "Populacao" : atributo2 == 2 ? "Area" : atributo2 == 3 ? "Densidade" : "PIB",
-           valor2_attr2,
-           soma2);
-
-    if (soma1 > soma2)
-        printf("\n%s venceu a rodada!\n", carta1.nome);
-    else if (soma2 > soma1)
-        printf("\n%s venceu a rodada!\n", carta2.nome);
+    // Vencedor ou empate
+    if(soma1 > soma2)
+        printf("Carta 1 venceu!\n");
+    else if(soma2 > soma1)
+        printf("Carta 2 venceu!\n");
     else
-        printf("\nEmpate!\n");
+        printf("Empate!\n");
 
     return 0;
 }
+
